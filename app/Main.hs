be4c4dc207f5eab630fc438aacd5d0a95d1b2053
Sean Lee <freshdried@gmail.com>
2016-02-24 12:51:38 -0500

    reflex proof of concept

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ExplicitForAll #-}
+
+
 module Main where
 
 import           Control.Monad
@@ -6,7 +11,53 @@ import           Language.Haskell.Exts.Parser
 import           Language.Haskell.Exts.Syntax
 import qualified Language.Haskell.Interpreter as Hint
 import qualified Text.Show.Pretty as Pr
+import           Control.Concurrent (forkIO)
+import           Reflex.Dom
+import           Reflex.Dom.Class
+import           Control.Monad.IO.Class
+import           Control.Monad.Fix (MonadFix)
+import           Data.Function
 
+
+
+main :: IO ()
+main = mainWidget myWidget
+
+
+myWidget :: MonadWidget t m => m ()
+myWidget = el "div" $ do
+
+  txtIn      :: TextInput t        <- textInput def
+  buttonEv   :: Event t ()         <- button "Submit"
+  let textEv :: Event t String      = tagDyn (_textInput_value txtIn) buttonEv
+
+  textDyn    :: Dynamic t String   <- holdDyn "enter something" textEv
+
+  el "div" $ do
+    el "h2" $ text "input:"
+    dynText textDyn
+
+  el "div" $ do
+    el "h2" $ text "type sig:"
+    tyEv    :: Event t String <- asyncMapIO f textEv
+    tyDyn   :: Dynamic t String <- holdDyn "(type signature)" tyEv
+    dynText tyDyn
+    return ()
+  return ()
+
+
+
+f :: String -> IO String
+f x = fmap g (hrun . Hint.typeOf $ x)  where
+    g (Left e) = show e
+    g (Right str) = str
+  
+
+
+
+
+
+-- Parsing
 
 typeOfAST :: Typeable a => a -> ParseResult Type
 typeOfAST = parseType . show . typeOf
@@ -16,15 +67,18 @@ foo = parseType "a -> a"
 
 
 hrun :: Hint.Interpreter a -> IO (Either Hint.InterpreterError a)
-hrun x = Hint.runInterpreter $
-  Hint.setImports ["Prelude"] >> x
+hrun x = Hint.runInterpreter 
+  $ Hint.setImports ["Prelude"] 
+  >> x
 
 
-main :: IO ()
-main = forever $ do
+
+
+mon :: IO ()
+mon = forever $ do
   putStr "λλλ: "
   l <- getLine
-  elt <- hrun . Hint.typeOf $ l
+  elt :: Either Hint.InterpreterError String <- hrun . Hint.typeOf $ l
   case elt of
     Left _ -> putStrLn "error!"
     Right lt -> do
@@ -33,3 +87,24 @@ main = forever $ do
       putStrLn lt
       putStrLn ""
       putStrLn $ Pr.ppShow lt_ast
+
+
+
+
+
+
+
+
+-- utils
+
+infixl 1 <&>
+(<&>) = flip fmap
+
+-- From https://github.com/reflex-frp/reflex-dom/pull/11/files
+asyncMapIO :: (MonadWidget t m) => (a -> IO b) -> Event t a -> m (Event t b)
+asyncMapIO f event = performEventAsync eActions
+     where eActions = fmap (forkApply f) event
+           forkApply f val callback = liftIO . void . forkIO $ f val >>= callback
+  
+say :: (MonadIO m) => String -> m ()
+say = liftIO . putStrLn
